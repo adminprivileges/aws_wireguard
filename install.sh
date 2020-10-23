1b454674398086ada623ac4cf98d871f6650c2cc
DOOM <caldwellthaddeus@protonmail.com>
2020-10-23 08:08:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+#Installing required packages
+apt update -y
+apt upgrade -y
+apt install wireguard qrencode unbound unbound-host curl -y
 echo "Pulling the files from github"
 curl https://raw.githubusercontent.com/adminprivileges/aws_wireguard/main/tc_wireguard.sh -o /etc/wireguard/tc_wireguard.sh
 curl https://raw.githubusercontent.com/adminprivileges/aws_wireguard/main/add_user.sh -o /etc/wireguard/tc_add_user.sh
