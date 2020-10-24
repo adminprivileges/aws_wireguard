e7a7cecbc251877302ffef9e5c90c6e54963e928
adminprivileges <caldwellthaddeus@protonmail.com>
2020-10-24 17:09:26 -0400

    Updating to use tc_wireguard_1 instead

--- install.sh
+++ install.sh
@@ -4,13 +4,7 @@ echo "Updating and installing required packages"
 apt update -y
 apt upgrade -y
 apt install wireguard qrencode curl -y
-echo "Pulling the files from github"
-curl https://raw.githubusercontent.com/adminprivileges/aws_wireguard/main/tc_wireguard.sh -o /etc/wireguard/tc_wireguard.sh
-curl https://raw.githubusercontent.com/adminprivileges/aws_wireguard/main/add_user.sh -o /etc/wireguard/tc_add_user.sh
-echo "Making files executable"
-chmod +x /etc/wireguard/tc_wireguard.sh
-chmod +x /etc/wireguard/tc_add_user.sh
 echo "Installing Wireguard"
-/etc/wireguard/tc_wireguard.sh
+./tc_wireguard_1.sh
 echo "Adding a user"
-/etc/wireguard/tc_add_user.sh
+./tc_add_user.sh
